78b1dd6b726c4fab9a5daa82fbf72e32a767f755
Eduardooliveira95 <eduardooliveirafo@sptech.school>
2023-03-14 19:44:07 -0300

    Ajustes atributos, CRUD finalizado

--- apl-back-zup/apl-back-zup/src/main/java/zup/finance/aplbackzup/UsuarioComum.java
+++ apl-back-zup/apl-back-zup/src/main/java/zup/finance/aplbackzup/UsuarioComum.java
@@ -3,14 +3,6 @@ package zup.finance.aplbackzup;
 public class UsuarioComum extends Usuario {
     private String cpf;
 
-    public UsuarioComum(String nome, String email, String username, String senha, boolean isInfluencer, Boolean autenticado, String cpf) {
-        super(nome, email, username, senha, isInfluencer, autenticado);
-        this.cpf = cpf;
-    }
-
-    public UsuarioComum() {
-    }
-
     public String getCpf() {
         return cpf;
     }
--- apl-back-zup/apl-back-zup/src/main/java/zup/finance/aplbackzup/UsuarioEmpresa.java
+++ apl-back-zup/apl-back-zup/src/main/java/zup/finance/aplbackzup/UsuarioEmpresa.java
@@ -3,14 +3,6 @@ package zup.finance.aplbackzup;
 public class UsuarioEmpresa extends Usuario {
     private String cnpj;
 
-    public UsuarioEmpresa(String nome, String email, String username, String senha, boolean isInfluencer, Boolean autenticado, String cnpj) {
-        super(nome, email, username, senha, isInfluencer, autenticado);
-        this.cnpj = cnpj;
-    }
-
-    public UsuarioEmpresa() {
-    }
-
     public String getCnpj() {
         return cnpj;
     }
