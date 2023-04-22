5962fb35786025eff5d931a2259bc99e19566627
Eduardooliveira95 <eduardooliveirafo@sptech.school>
2023-04-21 21:47:47 -0300

    Correção Endpoint

--- zup/zup/zup/src/main/java/school/sptech/zup/controller/ListController.java
+++ zup/zup/zup/src/main/java/school/sptech/zup/controller/ListController.java
@@ -4,10 +4,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import school.sptech.zup.dto.obj.UsuarioObj;
 import school.sptech.zup.service.UsuarioService;
 
@@ -22,7 +19,7 @@ public class ListController {
     private UsuarioObj[] vetor;
     private int nroElem;
 
-    @GetMapping("/{nomeArquivo}")
+    @PostMapping("/{nomeArquivo}")
     public ResponseEntity<Object>  gravarArquivoCsv(@PathVariable String nomeArquivo) {
         var retorno = _usuarioService.getListUsuario();
         if (retorno.getStatusCodeValue() == 200){
