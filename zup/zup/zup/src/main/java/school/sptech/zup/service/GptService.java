d7bf77a8fef5e5f99613416edb6e8f8c11c7e09c
Eduardooliveira95 <eduardooliveirafo@sptech.school>
2023-05-21 20:34:35 -0300

    Atualzação Chave Key

--- zup/zup/zup/src/main/java/school/sptech/zup/service/GptService.java
+++ zup/zup/zup/src/main/java/school/sptech/zup/service/GptService.java
@@ -7,12 +7,13 @@ import org.springframework.stereotype.Service;
 import school.sptech.zup.domain.Gpt;
 import school.sptech.zup.domain.Noticia;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.concurrent.CompletionService;
 
 @Service
 public class GptService {
-    private static final String API_KEY = "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";
+    private static final String API_KEY = "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";
 
     public List<CompletionChoice> gptNoticia(List<Noticia> noticias, Gpt gpt) {
         OpenAiService service = new OpenAiService(API_KEY);
@@ -22,6 +23,8 @@ public class GptService {
                 .prompt(noticias.get(gpt.getId()) + gpt.getPergunta())
                 .maxTokens(1000)
                 .build();
+
+
         return service.createCompletion(request).getChoices();
     }
 }
