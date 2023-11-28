ca22153883fa047a5c87994e0d164d5638ca1ef8
Eduardooliveira95 <eduardooliveirafo@sptech.school>
2023-11-28 00:21:28 -0300

    Alterando tamanho

--- src/main/java/school/sptech/zup/dto/request/FotoRequest.java
+++ src/main/java/school/sptech/zup/dto/request/FotoRequest.java
@@ -3,8 +3,11 @@ package school.sptech.zup.dto.request;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.Column;
+
 @Data
 @NoArgsConstructor
 public class FotoRequest {
+    @Column(length = 50 * 1024 * 1024)
     private byte[] foto;
 }
--- src/main/java/school/sptech/zup/dto/response/FotoResponse.java
+++ src/main/java/school/sptech/zup/dto/response/FotoResponse.java
@@ -3,8 +3,11 @@ package school.sptech.zup.dto.response;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.Column;
+
 @Data
 @NoArgsConstructor
 public class FotoResponse {
+    @Column(length = 50 * 1024 * 1024)
     private byte[] foto;
 }
