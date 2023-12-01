32c4d469a900eef133142f4077a3d607d87890bf
Eduardooliveira95 <eduardooliveirafo@sptech.school>
2023-12-01 01:10:59 -0300

    Teste 3

--- src/main/java/school/sptech/zup/dto/response/PerfilUsuarioResponse.java
+++ src/main/java/school/sptech/zup/dto/response/PerfilUsuarioResponse.java
@@ -4,11 +4,12 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 import school.sptech.zup.domain.Usuario;
 
+import javax.persistence.Column;
+
 @Data
 public class PerfilUsuarioResponse {
     @Schema(name = "nome", description = "Seu nome", example = "Carlos")
     private String nome;
-    private byte[] foto;
     @Schema(name = "idPerfil", description = "Perfis do usuário: 1 - Conversador; 2 - Moderado; 3- Agressivo", example = "Moderado")
     private Long IdPerfil;
     @Schema(name = "LinkYoutube", description = "Youtube do Influencer", example = "Primo Rico")
@@ -19,4 +20,6 @@ public class PerfilUsuarioResponse {
     private String LinkTikTok;
     @Schema(name = "DescPerfil", description = "Perfis do usuário: 1 - Conversador; 2 - Moderado; 3- Agressivo", example = "Moderado")
     private String DescPerfil;
+    @Column(length = 50 * 1024 * 1024)
+    private byte[] foto;
 }
